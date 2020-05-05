b6dd0aeee72c553b414303043a0d3163bc3d98b5
Gabor <renyhart.gabor@gmail.com>
2020-05-05 03:48:04 +0200

    Dátum bekérés, szoba elérhetőség vizsgálata

--- Progtech_szalloda.java
+++ Progtech_szalloda.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package progtech_szalloda;
-
+import  java.util.Scanner;
+import java.util.Random;
 /**
  *
  * @author renyh
@@ -19,6 +20,19 @@ public class Progtech_szalloda {
         String sznev=Szalloda.getSzallodanev();
         String szcim=Szalloda.getSzallodacim();
         System.out.println("Üdvözlöm az "+sznev+" oldalán\n"+szcim);
+        System.out.print("Mikorra szeretne szobát kivenni? (Formátum: 20200425): ");
+        Scanner sc;
+        sc=new Scanner(System.in);
+        String datum;
+        datum=sc.nextLine();
+        Random rand=new Random();
+        int random_szam = rand.nextInt(11);
+        if(random_szam>8)
+            System.out.println("Sajnos erre a napra nincs szabad szoba! Válasszon másik dátumot!");
+        else
+           System.out.print("Az ön által kiválasztott dátum: "+datum);
+         
+        
     }
     
 }
